a03e9cfde2961b685b01f6827502471edf598c05
BorisAnastasov <boris_ywu@abv.bg>
2023-08-27 09:21:46 +0300

    commit

--- Softuniada - 2016 (Practice)/Tasks from 2016/02. Draw a House/Program.cs
+++ Softuniada - 2016 (Practice)/Tasks from 2016/02. Draw a House/Program.cs
@@ -9,14 +9,9 @@ namespace _02._Draw_a_House
               {
                      int n = int.Parse(Console.ReadLine());
 
-                     StringBuilder sb = new StringBuilder();
+                     string result = CreatitingRoof(n, 2 * n - 1) + "\n" + CreatitingBlock(n, 2 * n - 1);
 
-
-                     // roof
-                     sb.AppendLine(CreatitingRoof(n, 2 * n - 1));
-                     sb.AppendLine(CreatitingBlock(n, 2 * n - 1));
-
-
+                     Console.WriteLine(result);
               }
 
               static string CreatitingRoof(int rows, int cols)
@@ -25,44 +20,43 @@ namespace _02._Draw_a_House
 
                      //making each floor
                      int count = 0;
-                     for (int i = 0; i < rows; i++)
+                     for (int i = 0; i < rows - 1; i++)
                      {
-                            for (int z = 0; z < cols; z++)
-                            {
-                                   result += new string('.', rows - count);
-                            }
+                            result += new string('.', rows - count - 1);
                             if (count == 0)
                             {
                                    result += "*";
                             }
-                            else{
-                                   result += "*";
-                                   result += new string(' ', count);
-                                   result += "*";
-                            }
-                            for (int z = 0; z < cols; z++)
+                            else
                             {
-                                   result += new string('.', rows - count);
+                                   result += "*";
+                                   result += new string(' ', 2*count-1);
+                                   result += "*";
                             }
+                            result += new string('.', rows - count - 1);
                             result += "\n";
+                            count++;
                      }
+                     char[] arr = new string('*', rows).ToCharArray();
+                     result += string.Join(" ", arr);
 
                      return result;
               }
-
 
               static string CreatitingBlock(int rows, int cols)
               {
                      string result = "";
 
+                     result += "+" + new string('-', cols - 2) + '+';
+                     result += "\n";
                      //making each floor
-                     for (int i = 0; i < rows; i++)
+                     for (int i = 0; i < rows-2; i++)
                      {
-                            for (int z = 0; z < cols; z++)
-                            {
+                            result += '|' + new string(' ', cols-2) +'|';
+                            result += "\n";
 
-                            }
                      }
+                     result += "+" + new string('-', cols - 2) + '+';
 
                      return result;
               }
