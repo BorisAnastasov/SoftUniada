48e81dc2a115dfcd83256003cb4a5cc7575bc828
BorisAnastasov <boris_ywu@abv.bg>
2023-08-29 21:52:26 +0300

    commit

---
+++ Softuniada - 2016 (Practice)/Tasks from 2016/03. Sum Time/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace _03._Sum_Time
+{
+       internal class Program
+       {
+              static void Main(string[] args)
+              {
+                     string inputOne = Console.ReadLine();
+                     string inputTwo = Console.ReadLine();
+
+
+
+              }
+       }
+}
