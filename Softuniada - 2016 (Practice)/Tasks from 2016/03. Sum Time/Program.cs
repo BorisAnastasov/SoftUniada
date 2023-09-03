80cec18861863865fe307e1fa1028c111bdcc862
BorisAnastasov <boris_ywu@abv.bg>
2023-09-03 18:39:20 +0300

    task 3 solved

--- Softuniada - 2016 (Practice)/Tasks from 2016/03. Sum Time/Program.cs
+++ Softuniada - 2016 (Practice)/Tasks from 2016/03. Sum Time/Program.cs
@@ -1,5 +1,7 @@
 ﻿using System;
+using System.Linq;
 using System.Text.RegularExpressions;
+
 namespace _03._Sum_Time
 {
        internal class Program
@@ -9,28 +11,47 @@ namespace _03._Sum_Time
                      string inputOne = Console.ReadLine();
                      string inputTwo = Console.ReadLine();
 
+                     int sum = ParsingTimeIntoMinutes(inputOne) + ParsingTimeIntoMinutes(inputTwo);
 
+                     Console.WriteLine(ConvertingToClock(sum));
               }
 
-              public static int ParsingToMinutes(string input)
+              static int ParsingTimeIntoMinutes(string input)
               {
-                     string pattern = @"(?<days>\d*):?:?(?<hours>\d*):(?<minutes>\d*)";
-                     string patternDays = @"^()";
-                     string patternHours = @"(?<days>\d*):?:?(?<hours>\d*):(?<minutes>\d*)";
-                     string patternMinutes = @"(?<days>\d*):?:?(?<hours>\d*):(?<minutes>\d*)";
-                     Match m = Regex.Match(input, pattern);
-                     int minutes = 0;
-                     if (m.Groups["days"].Success)
+                     int[] arr = input.Split(':').Where(s=>s.Length>0).Select(int.Parse).ToArray();
+                     int sum = 0;
+                     if (arr.Length == 3)
                      {
-                            int days = Regex.Match(m.Groups["days"], patternDays)
-                            minutes += 
-                     }
-                     //hours
-                     int hours = Regex.Match(m.Groups["days"], patternDays)
-                     minutes += hours * 60;
+                            sum += arr[0] * 1440;
+                            sum += arr[2] + arr[1] * 60;
 
-                     //minutes  
-                     minutes += int.Parse(m.Groups["minutes"]);
+                     }
+                     else
+                     {
+                            sum += arr[1] + arr[0] * 60;
+                     }
+
+                     return sum;
+              }
+
+              static string ConvertingToClock(int min)
+              {
+                     int days = min / 1440;
+                     min %= 1440;
+
+                     int hours = min / 60;
+                     min %= 60;
+
+                     int minutes = min;
+
+                     if (days > 0)
+                     {
+                            return $"{days}::{hours}:{minutes:D2}";
+                     }
+                     else
+                     {
+                            return $"{hours}:{minutes:D2}";
+                     }
               }
        }
 }
