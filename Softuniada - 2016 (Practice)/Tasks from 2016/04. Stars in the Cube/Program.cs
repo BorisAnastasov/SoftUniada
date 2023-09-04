ef3d11984bc087e6dae05863e39aa4ff921e7958
BorisAnastasov <boris_ywu@abv.bg>
2023-09-04 11:34:02 +0300

    task 4 solved

--- Softuniada - 2016 (Practice)/Tasks from 2016/04. Stars in the Cube/Program.cs
+++ Softuniada - 2016 (Practice)/Tasks from 2016/04. Stars in the Cube/Program.cs
@@ -1,82 +1,83 @@
-﻿using System.Linq;
-using System.Reflection.Emit;
-using System.Text;
-using System;
-namespace _04._Stars_in_the_Cube
-{
-       internal class Program
+﻿       using System.Linq;
+       using System.Reflection.Emit;
+       using System.Text;   
+       using System;
+       using System.Collections.Generic;
+       namespace _04._Stars_in_the_Cube
        {
-              static void Main(string[] args)
+              internal class Program
               {
-                     int n = int.Parse(Console.ReadLine());
-                     if (n < 3)
+                     static void Main(string[] args)
                      {
-                            Console.WriteLine(0);
-                            return;
-                     }
-
-                     char[,,] matrix = new char[n, n, n];
-
-                     Dictionary<char, int> dict = new Dictionary<char, int>();
-
-                     for (int i = 0; i < n; i++)
-                     {
-                            string[] arr = Console.ReadLine().Split(" | ").ToArray();
-                            for (int z = 0; z < n; z++)
+                            int n = int.Parse(Console.ReadLine());
+                            if (n < 3)
                             {
-                                   char[] ch = arr[z].Split(" ").Select(char.Parse).ToArray();
-                                   for (int o = 0; o < n; o++)
+                                   Console.WriteLine(0);
+                                   return;
+                            }
+
+                            char[,,] matrix = new char[n, n, n];
+
+                            Dictionary<char, int> dict = new Dictionary<char, int>();
+
+                            for (int i = 0; i < n; i++)
+                            {
+                                   string[] arr = Console.ReadLine().Split(new string[] { " | " },StringSplitOptions.None).ToArray();
+                                   for (int z = 0; z < n; z++)
                                    {
-                                          matrix[i, z, o] = ch[o];
+                                          char[] ch = arr[z].Split(" ").Select(char.Parse).ToArray();
+                                          for (int o = 0; o < n; o++)
+                                          {
+                                                 matrix[i, z, o] = ch[o];
+                                          }
                                    }
                             }
-                     }
 
-                     for (int row = 1; row < matrix.GetLength(0) - 1; row++)
-                     {
-                            for (int col = 1; col < matrix.GetLength(1) - 1; col++)
+                            for (int row = 1; row < matrix.GetLength(0) - 1; row++)
                             {
-                                   for (int lay = 1; lay < matrix.GetLength(2) - 1; lay++)
+                                   for (int col = 1; col < matrix.GetLength(1) - 1; col++)
                                    {
-                                          char currVal = matrix[row, col, lay];
-                                          if (CheckingCell(matrix, row, col, lay))
+                                          for (int lay = 1; lay < matrix.GetLength(2) - 1; lay++)
                                           {
-                                                 if (dict.ContainsKey(currVal))
+                                                 char currVal = matrix[row, col, lay];
+                                                 if (CheckingCell(matrix, row, col, lay))
                                                  {
-                                                        dict[currVal]++;
+                                                        if (dict.ContainsKey(currVal))
+                                                        {
+                                                               dict[currVal]++;
 
-                                                 }
-                                                 else
-                                                 {
-                                                        dict.Add(currVal, 1);
+                                                        }
+                                                        else
+                                                        {
+                                                               dict.Add(currVal, 1);
+                                                        }
                                                  }
                                           }
                                    }
                             }
+                            StringBuilder sb = new StringBuilder();
+                            int sum = 0;
+                            foreach (char key in dict.Keys.OrderBy(x => x))
+                            {
+                                   sb.AppendLine($"{key} -> {dict[key]}");
+                                   sum += dict[key];
+                            }
+                            Console.WriteLine(sum);
+                            Console.WriteLine(sb.ToString());
                      }
-                     StringBuilder sb = new StringBuilder();
-                     int sum = 0;
-                     foreach (char key in dict.Keys.OrderBy(x => x))
+
+
+                     public static bool CheckingCell(char[,,] matrix, int row, int col, int layer)
                      {
-                            sb.AppendLine($"{key} -> {dict[key]}");
-                            sum += dict[key];
+                            char currVal = matrix[row, col, layer];
+
+                            return matrix[row - 1, col, layer] == currVal && matrix[row + 1, col, layer] == currVal &&
+
+                            matrix[row, col - 1, layer] == currVal && matrix[row, col + 1, layer] == currVal &&
+
+                            matrix[row, col, layer - 1] == currVal && matrix[row, col, layer + 1] == currVal;
                      }
-                     Console.WriteLine(sum);
-                     Console.WriteLine(sb.ToString());
+
+
               }
-
-
-              public static bool CheckingCell(char[,,] matrix, int row, int col, int layer)
-              {
-                     char currVal = matrix[row, col, layer];
-
-                     return matrix[row - 1, col, layer] == currVal && matrix[row + 1, col, layer] == currVal &&
-
-                     matrix[row, col - 1, layer] == currVal && matrix[row, col + 1, layer] == currVal &&
-
-                     matrix[row, col, layer - 1] == currVal && matrix[row, col, layer + 1] == currVal;
-              }
-
-
        }
-}
